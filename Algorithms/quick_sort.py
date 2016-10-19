6b3d48e83f4486e23c924fa3661eb05dd9cbb14a
Sujit Roy <roy.sujit6@gmail.com>
2016-10-19 15:42:31 +0530

    Create quick_sort.py

---
+++ Algorithms/quick_sort.py
@@ -0,0 +1,8 @@
+def quicksort(myList, start, end):
+    if start < end:
+        # partition the list
+        pivot = partition(myList, start, end)
+        # sort both halves
+        quicksort(myList, start, pivot-1)
+        quicksort(myList, pivot+1, end)
+    return myList
