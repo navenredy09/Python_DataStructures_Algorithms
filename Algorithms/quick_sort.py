40704fa58a9ee90dcaf8c125d1d35b2cd1467e07
Sujit Roy <roy.sujit6@gmail.com>
2016-10-19 16:39:45 +0530

    Update quick_sort.py

--- Algorithms/quick_sort.py
+++ Algorithms/quick_sort.py
@@ -6,3 +6,30 @@ def quicksort(myList, start, end):
         quicksort(myList, start, pivot-1)
         quicksort(myList, pivot+1, end)
     return myList
+
+def partition(myList, start, end):
+    pivot = myList[start]
+    left = start+1
+    right = end
+    done = False
+    while not done:
+        while left <= right and myList[left] <= pivot:
+            left = left + 1
+        while myList[right] >= pivot and right >=left:
+            right = right -1
+        if right < left:
+            done= True
+        else:
+            # swap places
+            temp=myList[left]
+            myList[left]=myList[right]
+            myList[right]=temp
+    # swap start with myList[right]
+    temp=myList[start]
+    myList[start]=myList[right]
+    myList[right]=temp
+    return right
+
+myList=[3,1,5,4,3,7,87,32,4,634,32]
+quicksort(myList,0,10)
+print myList
