e1f718b79574fcaf2666fe035e2ba9aac43fc0c0
Sujit Roy <roy.sujit6@gmail.com>
2016-10-20 13:08:11 +0530

    Add files via upload

---
+++ Data-Structures/Queue.py
@@ -0,0 +1,21 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0,item)
+    def dequeue(self):
+        return self.items.pop()
+    def size(self):
+        return len(self.items)
+
+if __name__ == "__main__":
+    q = Queue()
+    q.enqueue('hello')
+    q.enqueue('dog')
+    q.enqueue(3)
+
+    print q.dequeue()
