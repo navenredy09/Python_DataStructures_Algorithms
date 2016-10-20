80f7b377e6d311aa8c592187b0256600cca80824
Sujit Roy <roy.sujit6@gmail.com>
2016-10-20 20:23:59 +0530

    Add files via upload

---
+++ Data-Structures/Deque.py
@@ -0,0 +1,57 @@
+#Dequeue implementation using OOP
+class Deque:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def addFront(self, item):
+        self.items.append(item)
+
+    def addRear(self, item):
+        self.items.insert(0,item)
+
+    def removeFront(self):
+        return self.items.pop()
+
+    def removeRear(self):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
+
+########################################
+#from pythonds.basic.deque import Deque
+ # Palindrome Checker
+def palchecker(aString):
+    chardeque = Deque()
+
+    for ch in aString:
+        chardeque.addRear(ch)
+
+    stillEqual = True
+
+    while chardeque.size() > 1 and stillEqual:
+        first = chardeque.removeFront()
+        last = chardeque.removeRear()
+        if first != last:
+            stillEqual = False
+    return stillEqual
+
+if __name__ == "__main__":
+    d = Deque()
+    print d.isEmpty()
+    d.addRear(4)
+    d.addRear('dog')
+    d.addFront('cat')
+    d.addFront(True)
+    print d.size()
+    print d.isEmpty()
+    d.addRear(8.4)
+    print d.removeRear()
+    print d.removeFront()
+    print
+    print palchecker("lsdkgsfh")
+    print palchecker("radar")
+    
