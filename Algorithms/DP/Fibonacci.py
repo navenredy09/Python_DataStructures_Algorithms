fe3b49a5530a6d9063013ecfcc31e897f1c3816c
Sujit Roy <roy.sujit6@gmail.com>
2016-12-24 12:36:13 +0530

    Update Fibonacci.py

--- Algorithms/DP/Fibonacci.py
+++ Algorithms/DP/Fibonacci.py
@@ -3,3 +3,11 @@ Fibonacci Series:
 f_1 = f_2 = 1
 f_n = f_{n-1} + f_{n-2} for n > 2
 '''
+
+# A Direct implementation of this definition.
+def fib(n):
+  if n<=2:
+    return 1
+  else:
+    return fib(n-1) + fib(n-2)
+
