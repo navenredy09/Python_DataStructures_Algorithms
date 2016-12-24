cd5fb8e812e299b9c7dfc3c4e1a4e2f0656b00c1
Sujit Roy <roy.sujit6@gmail.com>
2016-12-24 13:36:56 +0530

    Update Fibonacci.py

--- Algorithms/DP/Fibonacci.py
+++ Algorithms/DP/Fibonacci.py
@@ -10,4 +10,16 @@ def fib(n):
     return 1
   else:
     return fib(n-1) + fib(n-2)
+  
+# Using DP
+def fib_dp(n):
+    fib_values = [0,1]
+    for i in range(2,n+1):
+        fib_values.append(fib_values[i-1] + fib_values[i-2])
+    return fib_values[n]
+
+if __name__ == "__main__":
+  fib(100)        #this algorithm seems to never terminate!
+  fib_dp(100)     #It's a magic
+  
 
